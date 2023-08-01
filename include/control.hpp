28e0de4583befbd84d74b8c9008059192332eda6
Wang Guoxian <1162739554@qq.com>
2023-08-01 09:47:46 +0800

    完善lift接口

--- include/control.hpp
+++ include/control.hpp
@@ -5,17 +5,31 @@ enum Control_State{
     OUTTAKE,
     STOP,
     ON,
-    OFF
+    OFF,
+};
+enum Lift_State{
+    UP,
+    MIDDLE,
+    DOWN,
+    STOP,
 };
 class Control {
 public:
     Control(const std::vector<int8_t> &intake_motor_ports,pros::motor_gearset_e_t intake_gearset,const int8_t &lift_motor_port,
     pros::motor_gearset_e_t lift_gearset,const int8_t lift_press_button_port,const std::vector<int8_t> &wings_ports,
     const std::vector<int8_t> &hanger_ports);
+    /**
+     * \param state set the state of the intakes
+     * \param speed speed of the intakes
+    */
     void set_intake(Control_State state,int speed);
-    void set_lift(int speed);
+    void set_lift(int speed,Lift_State state=DOWN);
     void set_wings(Control_State state);
     void set_hanger(Control_State state);
+
+    void set_lift_up_pos(double pos);
+
+    void set_lift_middle_pos(double pos);
 private:
     std::vector<pros::Motor> intake_motors;
     std::shared_ptr<pros::Motor> lift_motor;
@@ -24,4 +38,6 @@ private:
     std::shared_ptr<pros::ADIDigitalOut> hanger_arm;
     std::shared_ptr<pros::ADIDigitalOut> hanger_claw;
     std::shared_ptr<pros::ADIDigitalIn> lift_press_button;
+    double lift_up_pos;
+    double lift_middle_pos;
 };
--- src/control.cpp
+++ src/control.cpp
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 Control::Control(const std::vector<int8_t> &intake_motor_ports,pros::motor_gearset_e_t intake_gearset,const int8_t &lift_motor_port,
     pros::motor_gearset_e_t lift_gearset,const int8_t lift_press_button_port,const std::vector<int8_t> &wings_ports,
     const std::vector<int8_t> &hanger_ports){
@@ -17,8 +16,9 @@ Control::Control(const std::vector<int8_t> &intake_motor_ports,pros::motor_gears
   hanger_arm=std::make_shared<pros::ADIDigitalOut>(abs(hanger_ports[0]),util::is_reversed(hanger_ports[0]));
   hanger_claw=std::make_shared<pros::ADIDigitalOut>(abs(hanger_ports[1]),util::is_reversed(hanger_ports[1]));
 
+  lift_up_pos=100.0;
+  lift_middle_pos=1600;
 }
-
 
 /**
  * \param state true for intake, false for outtake
@@ -35,15 +35,20 @@ void Control::set_intake(Control_State state,int speed){
   for(auto &intake_motor:intake_motors){
     intake_motor.move(state==INTAKE?speed:-speed);
   }
+
 }
 
 /**
  * \param state true for lift up, false for lift down
  * \param speed speed of the lift
 */
-void Control::set_lift(int speed){
+void Control::set_lift(int speed,Lift_State state) {
   // static pros::Motor lift_motor(10,pros::E_MOTOR_GEAR_200);
   // static pros::ADIDigitalIn press_buttion('H');
+  if(state==STOP){
+    lift_motor->brake();
+    lift_motor->set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+  }
   lift_motor->move(speed);
   pros::delay(100);
   int cnt=0;
@@ -53,17 +58,22 @@ void Control::set_lift(int speed){
       printf("lift time out\n");
       break;
     }
+
     if(lift_press_button->get_value()){
-      printf("press_button.get_value()=%d\n",cnt);
       cnt++;
       break;
     }
     if(cnt>=2)
       break;
-    
+
     pros::delay(1);
   }
-  // lift_motor.move(0);
+  lift.tare_position();
+  if(state==UP){
+    lift_motor->move_relative(lift_up_pos,speed);
+  }else if(state==MIDDLE){
+    lift_motor->move_relative(lift_middle_pos,speed);
+  }
   lift_motor->brake();
   lift_motor->set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
   return;
@@ -82,4 +92,12 @@ void Control::set_hanger(Control_State state){
   // static pros::ADIDigitalOut hanger2('B');
   hanger_arm->set_value(state==ON?true:false);
   hanger_claw->set_value(state==ON?true:false);
+}
+
+void Control::set_lift_up_pos(double pos){
+  lift_up_pos=pos;
+}
+
+void Control::set_lift_middle_pos(double pos){
+  lift_middle_pos=pos;
 }
