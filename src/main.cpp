8d1013b5fe78deaec2bd0093b045a2d412f39318
owmess <1162739554@qq.com>
2024-01-15 17:08:04 +0800

    update lemlib ver

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "EZ-Template/util.hpp"
 #include "control.hpp"
 #include "pros/misc.h"
-
+#include "lemlib/api.hpp"
 
 
 pros::Imu imu(12);
@@ -13,8 +13,8 @@ pros::Motor LB(3, pros::E_MOTOR_GEAR_600, true, pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor RF(8, pros::E_MOTOR_GEAR_600, false, pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor RM(9, pros::E_MOTOR_GEAR_600, true, pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor RB(10, pros::E_MOTOR_GEAR_600, true, pros::E_MOTOR_ENCODER_DEGREES);
-pros::MotorGroup left_group({LF,LM,LB});
-pros::MotorGroup right_group({RF,RM,RB});
+pros::Motor_Group left_group({LF,LM,LB});
+pros::Motor_Group right_group({RF,RM,RB});
 
 // 底盘构造
 Drive chassis=Drive(
@@ -42,6 +42,54 @@ Drive chassis=Drive(
   // 左右两侧轮组的距离(不使用陀螺仪控制底盘时需要用到该参数(英寸))
   ,10.5
 );
+
+// drivetrain settings
+lemlib::Drivetrain drivetrain(&left_group, // left motor group
+                              &right_group, // right motor group
+                              10.5, // 10 inch track width
+                              lemlib::Omniwheel::NEW_4, // using new 3.25" omnis
+                              300, // drivetrain rpm is 360
+                              2 // chase power is 2. If we had traction wheels, it would have been 8
+);
+
+// lateral motion controller
+lemlib::ControllerSettings linearController(10, // proportional gain (kP)
+                                            0, // integral gain (kI)
+                                            3, // derivative gain (kD)
+                                            3, // anti windup
+                                            1, // small error range, in inches
+                                            100, // small error range timeout, in milliseconds
+                                            3, // large error range, in inches
+                                            500, // large error range timeout, in milliseconds
+                                            20 // maximum acceleration (slew)
+);
+
+// angular motion controller
+lemlib::ControllerSettings angularController(2, // proportional gain (kP)
+                                             0, // integral gain (kI)
+                                             10, // derivative gain (kD)
+                                             3, // anti windup
+                                             1, // small error range, in degrees
+                                             100, // small error range timeout, in milliseconds
+                                             3, // large error range, in degrees
+                                             500, // large error range timeout, in milliseconds
+                                             0 // maximum acceleration (slew)
+);
+
+// sensors for odometry
+// note that in this example we use internal motor encoders (IMEs), so we don't pass vertical tracking wheels
+lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel 1, set to null
+                            nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
+                            nullptr, // horizontal tracking wheel 1
+                            nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
+                            &imu // inertial sensor
+);
+
+// create the chassis
+lemlib::Chassis odom(drivetrain, linearController, angularController, sensors);
+
+
+
 
 pros::Motor intake_motor1(4, pros::E_MOTOR_GEAR_200, false, pros::E_MOTOR_ENCODER_DEGREES);
 pros::MotorGroup intake_motor_group({intake_motor1});
